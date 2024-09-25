aff01eee725cfa39c68e0490d45a5cf8aeb4aa7b
CalebFilip <filip0732caleb@gmail.com>
2024-09-25 16:25:54 -0400

    feat: finished main function

--- main.py
+++ main.py
@@ -16,25 +16,24 @@ cashier_instance = cashier.Cashier()
 
 def main():
     ###  write the rest of the codes ###
-    order = SandwichMachine(resources)
     while True:
         choice = input("What would you like? (small/ medium/ large/ off/ report): ")
         if choice == "off":
             quit()
         if choice == "report":
-            print(f"Bread: {order.machine_resources['bread']} slice(s)")
-            print(f"Ham: {order.machine_resources['ham']} slice(s)")
-            print(f"Cheese: {order.machine_resources['cheese']} ounce(s)")
+            print(f"Bread: {sandwich_maker_instance.machine_resources['bread']} slice(s)")
+            print(f"Ham: {sandwich_maker_instance.machine_resources['ham']} slice(s)")
+            print(f"Cheese: {sandwich_maker_instance.machine_resources['cheese']} ounce(s)")
         elif choice == "small" or choice == "medium" or choice == "large":
             sandwich = recipes[choice]
 
-            # I wanted to display the cost of the sandwich being ordered
+            # I wanted to display the cost of the sandwich being sandwich_maker_instanceed
             print(f"The {choice} sandwich is ${recipes[choice]['cost']}")
 
-            if order.check_resources(sandwich["ingredients"]):
-                payment = order.process_coins()
-                if order.transaction_result(payment, sandwich["cost"]):
-                    order.make_sandwich(choice, sandwich["ingredients"])
+            if sandwich_maker_instance.check_resources(sandwich["ingredients"]):
+                payment = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(payment, sandwich["cost"]):
+                    sandwich_maker_instance.make_sandwich(choice, sandwich["ingredients"])
         else:
             print("Invalid choice.")
 
