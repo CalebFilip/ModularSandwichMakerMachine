0965d13894564be032d9188cc645b0c7dde56edb
CalebFilip <filip0732caleb@gmail.com>
2024-09-25 16:21:38 -0400

    feat: transferred code from Assignment1 to new project

--- cashier.py
+++ cashier.py
@@ -5,9 +5,20 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        return (int(input("How many whole dollars? ")) * 1 +
+                int(input("How many half dollars? ")) * .5 +
+                int(input("How many quarters? ")) * .25 +
+                int(input("How many nickles? ")) * .05)
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins >= cost:
+            if cost - coins == 0:
+                return True
+            else:
+                print(f"Your change is ${coins - cost}")
+                return True
+        else:
+            print(f"Insufficient funds.")
+            return False
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import data
+import sandwich_maker
+import cashier
 from sandwich_maker import SandwichMaker
 from cashier import Cashier
 
@@ -6,14 +8,35 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = sandwich_maker.SandwichMaker(resources)
+cashier_instance = cashier.Cashier()
 
 
 
 
 def main():
     ###  write the rest of the codes ###
+    order = SandwichMachine(resources)
+    while True:
+        choice = input("What would you like? (small/ medium/ large/ off/ report): ")
+        if choice == "off":
+            quit()
+        if choice == "report":
+            print(f"Bread: {order.machine_resources['bread']} slice(s)")
+            print(f"Ham: {order.machine_resources['ham']} slice(s)")
+            print(f"Cheese: {order.machine_resources['cheese']} ounce(s)")
+        elif choice == "small" or choice == "medium" or choice == "large":
+            sandwich = recipes[choice]
+
+            # I wanted to display the cost of the sandwich being ordered
+            print(f"The {choice} sandwich is ${recipes[choice]['cost']}")
+
+            if order.check_resources(sandwich["ingredients"]):
+                payment = order.process_coins()
+                if order.transaction_result(payment, sandwich["cost"]):
+                    order.make_sandwich(choice, sandwich["ingredients"])
+        else:
+            print("Invalid choice.")
 
 if __name__=="__main__":
     main()
